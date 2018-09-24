9e3484227711794de56772b6adca6388bc8a2da9
felekechgeexx <42793788+felekechgeexx@users.noreply.github.com>
2018-09-23 21:44:38 -0400

    Add files via upload

---
+++ huckle_finn_code.py
@@ -0,0 +1,23 @@
+import random
+from textblob import TextBlob
+
+with open('hucklefinn.txt', 'r') as file:
+    text = file.read()
+
+blob = TextBlob(text)
+
+adjectives = []
+nouns = []
+
+for word,pos in blob.tags:
+    # print(word,pos)
+    if (pos == 'JJ'):
+        adjectives.append(word)
+    if (pos == 'NN'):
+        nouns.append(word)
+
+for i in range(30):
+    a = random.choice(adjectives)
+    n = random.choice(nouns)
+    print(a,n)   
+ 
