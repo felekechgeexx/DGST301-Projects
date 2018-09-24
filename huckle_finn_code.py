8db7226d93cdcf3af60bd6edbc7fd0932c7c1d2d
felekechgeexx <42793788+felekechgeexx@users.noreply.github.com>
2018-09-24 11:02:44 -0400

    Update huckle_finn_code.py

--- huckle_finn_code.py
+++ huckle_finn_code.py
@@ -12,11 +12,12 @@ with open('hucklefinn.txt', 'r') as file:
 # This converts the file contents into TextBlob called 
 blob = TextBlob(text)
 
-# This creates a list of adjectives and nouns
+# This creates a list of adjectives and nouns and verbs
 adjectives = []
 nouns = []
+verbs = []
 
-# This uses TextBlob to read through the whole text and check fo adjectives and nouns.
+# This uses TextBlob to read through the whole text and check fo adjectives, nouns, and verbs
 # and then adds them to the appropriate list. 
 for word,pos in blob.tags:
     # print(word,pos)
@@ -24,11 +25,18 @@ for word,pos in blob.tags:
         adjectives.append(word)
     if (pos == 'NN'):
         nouns.append(word)
+    if (pos == 'VB'):
+        verbs.append(word)
 
 # This will generate a twenty-line poem using randomly paired adjectives.
 # attached with a noun that is randomly generated five times and prints its pairs
 for i in range(20):
-    a = random.choice(adjectives)
-    n = random.choice(nouns)
-    print(a,n)   
- 
+    a1 = random.choice(adjectives)
+    a2 = random.choice(adjectives)
+    n1 = random.choice(nouns)
+    n2 = random.choice(nouns)
+    n3 = random.choice(nouns)
+    vb = random.choice(verbs)    
+    print(n1 + " " + a1 + " " +  vb  + " " + a2  + " " + n3) 
+    
+    
