80162e0d7c890d393699412c93d34b830f233807
felekechgeexx <42793788+felekechgeexx@users.noreply.github.com>
2018-09-24 08:42:49 -0400

    Update huckle_finn_code.py

--- huckle_finn_code.py
+++ huckle_finn_code.py
@@ -1,14 +1,23 @@
 import random
 from textblob import TextBlob
 
+# Textblob will for my file called "hucklefinn.txt"
+
+# in the same directory as my python notebook or script.
+
+# The text file's contents are stored in the variable "text"
 with open('hucklefinn.txt', 'r') as file:
     text = file.read()
-
+    
+# This converts the file contents into TextBlob called 
 blob = TextBlob(text)
 
+# This creates a list of adjectives and nouns
 adjectives = []
 nouns = []
 
+# This uses TextBlob to read through the whole text and check fo adjectives and nouns.
+# and then adds them to the appropriate list. 
 for word,pos in blob.tags:
     # print(word,pos)
     if (pos == 'JJ'):
@@ -16,6 +25,8 @@ for word,pos in blob.tags:
     if (pos == 'NN'):
         nouns.append(word)
 
+# This will generate a thirty-line poem using randomly paired adjectives.
+# attached with a noun that is randomly generated five times and prints its pairs
 for i in range(30):
     a = random.choice(adjectives)
     n = random.choice(nouns)
